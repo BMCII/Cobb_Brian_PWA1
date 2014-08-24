621a8757906adbb2c45ba5628afaf5ab81848c9f
BMCII <brian.m.cobbii@fullsail.edu>
2014-08-23 22:57:25 -0400

    class work
    class work

--- Class_activities/Head-First-JavaScript-Programming-master/chapter9/js/image.js
+++ Class_activities/Head-First-JavaScript-Programming-master/chapter9/js/image.js
@@ -2,17 +2,35 @@
  Brian Cobb
  */
 
-alert("testing 1,2,3");
 
 
-window.onload = init;
 
-function init() {
-    var image = document.getElementById("zero");
-    image.onclick = showAnswer;
+window.onload = init;                                               //run init function after window is loaded
+
+function init() {                                                   //declare function init
+
+    var images = document.getElementsByTagName("img");              //declare variable to hold images from document
+    for (var i = 0 ; i < images.length; i++) {                      //iterate through the images
+        images[i].onclick = showAnswer;                             //showAnswer onclick
+    }
+
+
 };
 
-function showAnswer() {
-    var image = document.getElementById("zero");
-    image.src = "zero.jpg";
+function showAnswer(clickEventObject) {                             //declare function showAnswer
+    var image = clickEventObject.target;                            //declare variable to hold image
+    var name = image.id;                                            //declare variable for image name by id
+    alert("you clicked on image " + name);                          //alert user of image clicked
+    name = "images/" + name + ".jpg";                               //change image path
+    image.src = name;                                               //change image source to var name
+    setTimeout(reblur, 2000, image);                                //set clock for reblur function
+
+
+}
+
+function reblur(image) {                                            //declare function reblur
+    var name = image.id;                                            //declare variable to hold images from document
+    name = "images/" + name + "blur.jpg";                           //change image path
+    image.src = name;                                               //change image source to var name
+    alert('"Congrats! Game Image ReBlured"');                       //alert user image reblured
 }
